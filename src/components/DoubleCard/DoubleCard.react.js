4fb8f66b3372746c9b228098e69bba7b070412eb
Sergio Navarro Guillamón <sergio.navarro@thelogicvalue.com>
2022-04-08 09:02:39 +0200

    -Refactor DoubleCard

--- src/components/DoubleCard/DoubleCard.react.js
+++ src/components/DoubleCard/DoubleCard.react.js
@@ -2,19 +2,35 @@ import * as React from 'react';
 import cn from "classnames";
 import { Card, Text } from "../";
 
-function DoubleCard({ className, title, firstValue, firstlabel, secondValue, secondlabel, compare = () => null, children }) {
+function DoubleCard({ className, title, firstValue, firstlabel, secondValue, secondlabel, classColorValue = null, children }) {
     const classes = cn(className);
+
+    const getColorClass = React.useCallback((value) => {
+        let color = null
+
+        Object.entries(classColorValue).forEach(([key, item]) => {
+            if (item(value)) color = key
+        })
+
+        return color
+    }, [])
 
     return (
         <Card className={classes}>
             <Text className="double-card-header">{title}</Text>
             <div className="double-card-body">
                 <div className="double-card-colum">
-                    <Text color={compare(firstValue, secondValue)} className="double-card-text"><b>{firstValue}</b></Text>
+                    {
+                        classColorValue ? <Text color={getColorClass(firstValue)} className="double-card-text"><b>{firstValue}</b></Text>
+                            : <Text className="double-card-text"><b>{firstValue}</b></Text>
+                    }
                     <Text className="double-card-subtext">{firstlabel}</Text>
                 </div>
                 <div className="double-card-colum">
-                    <Text className="double-card-text "><b>{secondValue}</b></Text>
+                    {
+                        classColorValue ? <Text color={getColorClass(secondValue)} className="double-card-text"><b>{secondValue}</b></Text>
+                            : <Text className="double-card-text"><b>{secondValue}</b></Text>
+                    }
                     <Text className="double-card-subtext">{secondlabel}</Text>
                 </div>
             </div>
