f0686cc4e1151137c5d8ce30addfe1433dd51b2c
sergioguarque <sergio.guarque@thelogicvalue.com>
2022-09-16 13:14:12 +0200

    retoques

--- src/components/AGGridTable/index.js
+++ src/components/AGGridTable/index.js
@@ -1,9 +1,3 @@
 // @flow
-
-import AGGridTable from "./AGGridTable.react"
-import { OverlayLoading } from "./AGGridTable.react"
-
-export {
-    AGGridTable as default,
-    OverlayLoading
-}
+import AGGridTable, { OverlayLoading } from "./AGGridTable.react"
+export { AGGridTable, OverlayLoading }
