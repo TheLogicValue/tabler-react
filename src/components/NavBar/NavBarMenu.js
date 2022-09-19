4565ecfb92100770a1084bcaf11ee0d17eefc8ff
sergioguarque <sergio.guarque@thelogicvalue.com>
2022-09-19 15:25:33 +0200

    fix menu

--- src/components/NavBar/NavBarMenu.js
+++ src/components/NavBar/NavBarMenu.js
@@ -4,9 +4,8 @@ import NavBarItem from "./NavBarItem"
 import NavBarDropDown from "./NavBarDropDown"
 import cn from "classnames"
 
-export default function NavBarMenu ({ item }) {
+export default function NavBarMenu({ items }) {
     const [dropdown, setDropdown] = useState(false)
-    const classes = cn("nav-item", { "show": dropdown }, item.className)
     const ref = useRef()
 
     useEffect(() => {
@@ -24,33 +23,40 @@ export default function NavBarMenu ({ item }) {
     }, [dropdown])
 
     return (
-        <li className={classes} ref={ref}>
-            {item.subItems ? (
-                <>
-                    <div
-                        aria-haspopup="menu"
-                        className="nav-link-item"
-                        aria-expanded={dropdown ? "true" : "false"}
-                        onClick={() => setDropdown((prev) => !prev)}
-                    >
-                        <Icon name={item.icon} />
-                        <span className="nav-link-title">
-                            {item.value}
-                        </span>
-                        <Icon name="chevron-down" className="text-right" />
-                    </div>
-                    <NavBarDropDown submenus={item.subItems} dropdown={dropdown} triggerEvent={() => setDropdown(false)} />
-                </>
-            ) : (
-                <NavBarItem
-                    LinkComponent={item.LinkComponent}
-                    className={item.className}
-                    to={item.to}
-                    menuItem={true}
-                    onClick={item.onClick}
-                >
-                    <Icon name={item.icon} />{item.value}
-                </NavBarItem>)}
-        </li>
+        <ul className="nav nav-tabs border-0 flex-column flex-lg-row">
+            {items.map((item, i) => {
+                const classes = (item?.subItems === undefined) ? "nav-item" : cn("nav-item", { "show": dropdown }, item.className)
+                return (
+                    <li className={classes} ref={ref}>
+                        {item.subItems ? (
+                            <>
+                                <div
+                                    aria-haspopup="menu"
+                                    className="nav-link-item"
+                                    aria-expanded={dropdown ? "true" : "false"}
+                                    onClick={() => setDropdown((prev) => !prev)}
+                                >
+                                    <Icon name={item.icon} />
+                                    <span className="nav-link-title">
+                                        {item.value}
+                                    </span>
+                                    <Icon name="chevron-down" className="text-right" />
+                                </div>
+                                <NavBarDropDown submenus={item.subItems} dropdown={dropdown} triggerEvent={() => setDropdown(false)} />
+                            </>
+                        ) : (
+                            <NavBarItem
+                                LinkComponent={item.LinkComponent}
+                                className={item.className}
+                                to={item.to}
+                                menuItem={true}
+                                onClick={item.onClick}
+                            >
+                                <Icon name={item.icon} />{item.value}
+                            </NavBarItem>)}
+                    </li>
+                )
+            })}
+        </ul>
     )
 }
