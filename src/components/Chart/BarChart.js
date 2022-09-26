c2d1e00485af4fcb07e6756c063b4e82d919c4ca
sergioguarque <sergio.guarque@thelogicvalue.com>
2022-09-26 13:38:39 +0200

    cambios para su uso en horizontal

--- src/components/Chart/BarChart.js
+++ src/components/Chart/BarChart.js
@@ -2,26 +2,77 @@ import React from 'react'
 import ReactECharts from 'echarts-for-react'
 
 export default function BarChart({
+    title,
+    type = "vertical",
     height,
+    colors,
+    complete,
+    rotateX,
+    //series
     series,
     seriesDataName,
     seriesName,
-    colors,
-    complete,
-    titleBarchart,
-    rotateX,   
-    name = "Export",
-    title = "",
-    download = false,    
+    seriesShowBackground = true,
+    //download
+    downloadName = "Export",
+    downloadTitle = "",
+    download = false,
+    //Methods    
     tooltipformatter,
     yAxisformatter,
     yAxisMinMax,
-    xAxisData
+    xAxisMinMax,
+    //data Axis
+    xAxisData,
+    yAxisData
 }) {
+
+    const xAxis = {
+        type: type === "vertical" ? "value" : "category",
+        data: xAxisData ?? []
+    }
+
+    const yAxis = {
+        type: type === "vertical" ? "Category" : "value",
+        data: yAxisData ?? []
+    }
+
+    if (type === "vertical") {
+        xAxis.axisLabel = { rotate: rotateX, verticalAlign: "top", fontSize: 9, interval: 0 }
+
+        yAxis.show = complete
+        yAxis.axisLabel = { formatter: (item) => yAxisformatter(item) }
+        yAxis.max = (value) => yAxisMinMax(value, "max")
+        yAxis.min = (value) => yAxisMinMax(value, "min")
+    }
+
+    if(type !== "vertical"){
+        xAxis.boundaryGap = [0, 0.01]
+        xAxis.max = (value) => xAxisMinMax(value, "max")
+        xAxis.min = (value) => xAxisMinMax(value, "min")
+    }
+
+    const grid = () => {
+        let verticalGrid = {
+            top: complete == true ? 30 : 10,
+            bottom: complete == true ? 70 : 0,
+            left: complete == true ? 60 : 0,
+            right: complete == true ? 60 : 0,
+        }
+        let horizontalGrid = {
+            left: '3%',
+            right: '4%',
+            top: '5%',
+            bottom: '25%',
+            containLabel: true
+        }
+        return type === "vertical" ? verticalGrid : horizontalGrid
+    }
+
     const option = {
-        title: { text: titleBarchart, left: 'center' },
+        title: { text: title, left: 'center' },
         toolbox: {
-            feature: { saveAsImage: { name: name, title: title, show: download } }
+            feature: { saveAsImage: { name: downloadName, title: downloadTitle, show: download } }
         },
         legend: { show: complete, bottom: "0rem", symbol: null },
         tooltip: {
@@ -29,30 +80,15 @@ export default function BarChart({
             axisPointer: { type: 'shadow' },
             formatter: (item) => tooltipformatter(item)
         },
-        xAxis: {
-            type: 'category',
-            data: xAxisData,
-            axisLabel: { rotate: rotateX, verticalAlign: "top", fontSize: 9, interval: 0 }
-        },
-        yAxis: {
-            show: complete,
-            type: 'value',
-            axisLabel: { formatter: (item) => yAxisformatter(item) },
-            max: (value) => yAxisMinMax(value, "max"),
-            min: (value) => yAxisMinMax(value, "min")
-        },
-        grid: {
-            top: complete == true ? 30 : 10,
-            bottom: complete == true ? 70 : 0,
-            left: complete == true ? 60 : 0,
-            right: complete == true ? 60 : 0,
-        },
+        xAxis: xAxis,
+        yAxis: yAxis,
+        grid: grid(),
         series: series.map((item, i) => ({
+            type: 'bar',
+            color: colors[i],
             data: item.slice(0, seriesDataName.length),
             name: seriesName[i],
-            color: colors[i],
-            type: 'bar',
-            showBackground: true,
+            showBackground: seriesShowBackground,
             backgroundStyle: { color: 'rgba(180, 180, 180, 0.2)' },
         }))
     }
