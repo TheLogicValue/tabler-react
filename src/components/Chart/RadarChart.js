a6650192330752c710fa0992c8fdbc0f96775040
sergioguarque <sergio.guarque@thelogicvalue.com>
2022-09-28 12:49:50 +0200

    renombrar RadarChart

--- src/components/Chart/RadarChart.js
+++ src/components/Chart/RadarChart.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactECharts from 'echarts-for-react'
 
-export default function RarChart({
+export default function RadarChart({
     title = "",   
     height = "18.75rem",
     paddingBottom = null,
