5a898ca67af493cde2cc3e6be8c89819bd1506ac
Sergio Navarro Guillamón <sergio.navarro@thelogicvalue.com>
2022-10-04 15:50:23 +0200

    -refactor.

--- src/components/NavBar/NavBarItem.js
+++ src/components/NavBar/NavBarItem.js
@@ -14,14 +14,8 @@ const NavBarItem = ({
     const route = to?.substring(1).split("/")
     const location = useLocation()
     const classes = cn("nav-link", { "nav-link-item": menuItem }, { "active": location.pathname.includes(route?.length > 1 && route[0]) }, className)
-    
-    if (LinkComponent) return <NavLink
-        to={to}
-        className={({ isActive }) => {
-            return cn("nav-link", { "nav-link-item": menuItem }, { "active": isActive || location.pathname.includes(route?.length > 1 && route[0]) }, className)
-        }}
-        end
-    >{value || children}</NavLink>
+
+    if (LinkComponent) return <NavLink to={to} className={classes} end>{value || children}</NavLink>
     else return <a className={classes} href={to} onClick={onClick}>{value || children}</a>
 }
 export default NavBarItem
