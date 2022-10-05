10c975b20c6e41e89b6372ede2af8fdf42592273
Sergio Navarro Guillamón <sergio.navarro@thelogicvalue.com>
2022-10-05 10:20:52 +0200

    -refactor menu.

--- src/components/NavBar/NavBarDropDown.js
+++ src/components/NavBar/NavBarDropDown.js
@@ -1,9 +1,42 @@
-import React from "react"
+import React, { useState, useEffect, useRef } from "react"
+import Icon from "../Icon"
 import NavBarItem from "./NavBarItem"
+import cn from "classnames"
 
-const NavBarDropDown = ({ submenus, dropdown, triggerEvent = () => null }) => {
-    return (
-        <ul className={`dropdown-menu dropdown-menu-left dropdown-menu-arrow ${dropdown ? "show" : ""}`} onClick={triggerEvent}>
+const NavBarDropDown = ({ value, submenus, icon }) => {
+    const [dropdown, setDropdown] = useState(false)
+    const ref = useRef()
+    const classes = cn("nav-link-item", { "show": dropdown })
+
+    useEffect(() => {
+        const handler = (event) => {
+            if (dropdown && ref.current && !ref.current.contains(event.target))
+                setDropdown(false)
+        }
+        document.addEventListener("mousedown", handler)
+        document.addEventListener("touchstart", handler)
+        return () => {
+            // Cleanup the event listener
+            document.removeEventListener("mousedown", handler)
+            document.removeEventListener("touchstart", handler)
+        }
+    }, [dropdown])
+
+    return <div ref={ref} key={value}>
+        <div
+            aria-haspopup="menu"
+            className={classes}
+            aria-expanded={dropdown ? "true" : "false"}
+            onClick={() => setDropdown((prev) => !prev)}
+
+        >
+            <Icon name={icon} />
+            <span className="nav-link-title">
+                {value}
+            </span>
+            <Icon name="chevron-down" className="text-right" />
+        </div>
+        <ul className={`dropdown-menu dropdown-menu-left dropdown-menu-arrow ${dropdown ? "show" : ""}`} onClick={() => setDropdown(false)}>
             {submenus.map(item => (
                 <NavBarItem
                     LinkComponent={item.LinkComponent}
@@ -18,6 +51,6 @@ const NavBarDropDown = ({ submenus, dropdown, triggerEvent = () => null }) => {
             )
             )}
         </ul>
-    )
+    </div>
 }
 export default NavBarDropDown
--- src/components/NavBar/NavBarMenu.js
+++ src/components/NavBar/NavBarMenu.js
@@ -1,51 +1,18 @@
-import React, { useState, useEffect, useRef } from "react"
+import React from "react"
 import Icon from "../Icon"
 import NavBarItem from "./NavBarItem"
 import NavBarDropDown from "./NavBarDropDown"
 import cn from "classnames"
 
 export default function NavBarMenu({ items }) {
-    const [dropdown, setDropdown] = useState(false)
-    const ref = useRef()
-
-    useEffect(() => {
-        const handler = (event) => {
-            if (dropdown && ref.current && !ref.current.contains(event.target))
-                setDropdown(false)
-        }
-        document.addEventListener("mousedown", handler)
-        document.addEventListener("touchstart", handler)
-        return () => {
-            // Cleanup the event listener
-            document.removeEventListener("mousedown", handler)
-            document.removeEventListener("touchstart", handler)
-        }
-    }, [dropdown])
-
     return (
         <ul className="nav nav-tabs border-0 flex-column flex-lg-row">
-            {items.map((item, i) => {
-                const classes = (item?.subItems === undefined) ? "nav-item" : cn("nav-item", { "show": dropdown }, item.className)
+            {items.map(item => {
+                const classes = cn("nav-item", item.className)
                 return (
-                    <li className={classes} ref={ref} key={item.value}>
-                        {item.subItems ? (
-                            <>
-                                <div
-                                    aria-haspopup="menu"
-                                    className="nav-link-item"
-                                    aria-expanded={dropdown ? "true" : "false"}
-                                    onClick={() => setDropdown((prev) => !prev)}
-                                >
-                                    <Icon name={item.icon} />
-                                    <span className="nav-link-title">
-                                        {item.value}
-                                    </span>
-                                    <Icon name="chevron-down" className="text-right" />
-                                </div>
-                                <NavBarDropDown submenus={item.subItems} dropdown={dropdown} triggerEvent={() => setDropdown(false)} />
-                            </>
-                        ) : (
-                            <NavBarItem
+                    <li className={classes} key={item.value}>
+                        {item.subItems ? <NavBarDropDown submenus={item.subItems} value={item.value} icon={item.icon}/>
+                            : <NavBarItem
                                 LinkComponent={item.LinkComponent}
                                 className={item.className}
                                 to={item.to}
@@ -53,7 +20,8 @@ export default function NavBarMenu({ items }) {
                                 onClick={item.onClick}
                             >
                                 <Icon name={item.icon} />{item.value}
-                            </NavBarItem>)}
+                            </NavBarItem>
+                        }
                     </li>
                 )
             })}
