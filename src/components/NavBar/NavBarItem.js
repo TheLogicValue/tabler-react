68f3e45ed095d36ab814bac456986a16ece4c69e
Sergio Navarro Guillamón <sergio.navarro@thelogicvalue.com>
2023-03-27 17:14:26 +0200

    -fix active route.

--- src/components/NavBar/NavBarItem.js
+++ src/components/NavBar/NavBarItem.js
@@ -13,7 +13,9 @@ const NavBarItem = ({
 }) => {
     const route = to?.substring(1).split("/")
     const location = useLocation()
-    const classes = cn("nav-link", { "nav-link-item": menuItem }, { "active": location.pathname.includes(route?.length > 1 && route[0]) }, className)
+    const classes = cn("nav-link", { "nav-link-item": menuItem }, { 
+        "active": location.pathname.includes(route[0]) 
+    }, className)
 
     if (LinkComponent) return <NavLink to={to} className={classes} end>{value || children}</NavLink>
     else return <a className={classes} href={to} onClick={onClick}>{value || children}</a>
