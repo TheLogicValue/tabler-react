c1459255035730d544341bb47245914e12f8f745
sergioguarque <sergio.guarque@thelogicvalue.com>
2024-11-25 12:41:19 +0100

    pasar el tipo de formato

--- src/components/Chart/DoughnutChart.js
+++ src/components/Chart/DoughnutChart.js
@@ -6,7 +6,7 @@ export default function DoughnutChart({
     height = "18.75rem",
     paddingBottom = null,
     legend = true,
-    legendFormatter,
+    legendFormat = '{c}',   
     colors,
     complete,
     //series
@@ -33,7 +33,7 @@ export default function DoughnutChart({
                 radius: ['30%', '70%'],
                 label: {
                     show: true,
-                    formatter: (item) => legendFormatter(item),
+                    format: legendFormat,
                     position: 'inside'
                 },
                 tooltip: {
