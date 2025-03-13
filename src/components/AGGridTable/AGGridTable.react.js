7e03ab46b02d8e2332c09047e3e69b507469acc8
Sergio Guarque <sergio.guarque@thelogicvalue.com>
2025-03-13 17:25:39 +0100

    retoques

--- src/components/AGGridTable/AGGridTable.react.js
+++ src/components/AGGridTable/AGGridTable.react.js
@@ -10,7 +10,7 @@ import 'ag-grid-community/styles/ag-theme-balham.css'
 // import { ModuleRegistry } from "@ag-grid-community/core"
 // ModuleRegistry.registerModules([ClientSideRowModelModule])
 import { ModuleRegistry } from "ag-grid-community";
-import { ClientSideRowModelModule } from "ag-grid-community/client-side-row-model";
+import { ClientSideRowModelModule } from "@ag-grid-community/client-side-row-model";
 import { AllEnterpriseModule, LicenseManager } from "ag-grid-enterprise";
 
 export function OverlayLoading(text) { return `<span class="ag-overlay-loading-center">${text}</span>` }
