d5f95022acd252aa0d92f5773cdd446e0bb61977
Eduard Jipa <eduard.jipa@thelogicvalue.com>
2025-11-17 12:25:49 +0100

    v2

--- src/components/AGGridTable/AGGridTable.react.js
+++ src/components/AGGridTable/AGGridTable.react.js
@@ -30,7 +30,7 @@ function configureAgGrid(licenseKey) {
 
 const AGGridTable = forwardRef((gridProps, ref) => {
 
-    console.log("Usando AGGridTable para modificar el filtro V1 "); 
+    console.log("Usando AGGridTable para modificar el filtro V2 "); 
 
     const {
         licenseKey = null,
@@ -109,12 +109,12 @@ const AGGridTable = forwardRef((gridProps, ref) => {
         if (minWidth != null) columnDef["minWidth"] = minWidth
 
         if (column.filter === "set" || column.filter === "agSetColumnFilter") {
-        columnDef.filter = "agSetColumnFilter"
-        columnDef.filterParams = {
-            ...columnDef.filterParams,
-            cellRenderer: IconSetFilterRenderer
+            columnDef.filter = "agSetColumnFilter"
+            columnDef.filterParams = {
+                ...columnDef.filterParams,
+                cellRendererFramework: IconSetFilterRenderer
+            }
         }
-    }
 
         return columnDef
     }), [dataColumn, flex, minWidth, resizable, sortable])
@@ -146,15 +146,20 @@ const AGGridTable = forwardRef((gridProps, ref) => {
         topGrid.api.exportDataAsCsv({ fileName: textFileCSV, columnSeparator: ";" })
     }
 
-   const IconSetFilterRenderer = (params) => {
-        
-    console.log(`Usando IconSetFilterRenderer en V1`);
+   const IconSetFilterRenderer = (props) => {
+    const { value, api, colDef } = props;
+    const isSelected = api.getFilterInstance(colDef.field).getModel()?.values?.includes(value);
 
-    const value = params.value;
-    return `<span style="display:flex; align-items:center;">
-                <span style="color:green; margin-right:4px;">✔</span>${value}
-            </span>`;
-    };
+    console.log("Usando IconSetFilterRenderer en V2", value, isSelected);
+
+    return (
+        <span style={{ display: "flex", alignItems: "center" }}>
+            <input type="checkbox" readOnly checked={isSelected} style={{ marginRight: "4px" }} />
+            {value}
+        </span>
+    );
+};
+
 
     useImperativeHandle(ref, () => {
         return {
