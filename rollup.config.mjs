e2d87c3944c22e19bb8ce9e3beed0aa3dbdff62b
Sergio Guarque <sergio.guarque@thelogicvalue.com>
2025-12-30 13:07:08 +0100

    retoques en algunas dependencias retoques en el compilado

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,36 +1,36 @@
-import babel from "rollup-plugin-babel";
-import commonjs from "@rollup/plugin-commonjs";
-import resolve from "@rollup/plugin-node-resolve";
-import url from "@rollup/plugin-url";
-import terser from "@rollup/plugin-terser";
-import external from "rollup-plugin-peer-deps-external";
-import postcss from "rollup-plugin-postcss";
+
+import babel from '@rollup/plugin-babel';
+import commonjs from '@rollup/plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
+import url from '@rollup/plugin-url';
+import external from 'rollup-plugin-peer-deps-external';
+import postcss from 'rollup-plugin-postcss';
+import terser from '@rollup/plugin-terser';
 
 const config = {
-  input: "src/index.js",
+  input: 'src/index.js',
   output: [
-    {
-      file: "dist/index.js",
-      format: "cjs",
-    },
-    {
-      file: "dist/index.es.js",
-      format: "es",
-    },
+    { file: 'dist/index.js', format: 'cjs', exports: 'named', sourcemap: true },
+    { file: 'dist/index.es.js', format: 'es', sourcemap: true },
   ],
+  external: ['react', 'react-dom', 'react-router-dom', '@popperjs/core', 'react-popper'],
   plugins: [
     external(),
-    postcss({
-      modules: false,
+    babel({
+      babelHelpers: 'runtime',
+      exclude: /node_modules/,
+      extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      presets: ['@babel/preset-env', '@babel/preset-flow'],
+      plugins: [
+        '@babel/plugin-transform-runtime',
+        '@babel/plugin-transform-json-strings',
+      ],
     }),
+    resolve({ extensions: ['.mjs', '.js', '.jsx', '.json', '.ts', '.tsx'] }),
+    commonjs(),
+    postcss({ modules: false }),
     url(),
     terser(),
-    babel({
-      exclude: "node_modules/**",
-      plugins: ["@babel/plugin-transform-json-strings"]
-    }),
-    resolve(),
-    commonjs(),
   ],
 };
 
--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,5 @@
 // @flow
-import type NotificationProps from "./Notification";
-export type { NotificationProps };
-
+export { default as Notification } from "./Notification";
 export { default as AccountDropdown } from "./AccountDropdown";
 export { AGGridTable, OverlayLoading, AGGridPagination } from "./AGGridTable";
 export { default as Alert } from "./Alert";
